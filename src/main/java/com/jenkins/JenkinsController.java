158ec586bb5c2ef343ce3828203651ae976ff251
krishna6677861 <krishna6677861@gmail.com>
2025-06-23 17:55:12 +0530

    succesful

---
+++ src/main/java/com/jenkins/JenkinsController.java
@@ -0,0 +1,25 @@
+package com.jenkins;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class JenkinsController {
+	@GetMapping(value="/get")
+	public String get() {
+		return "Hello World";
+	}
+	@GetMapping(value="/getvalue")
+	public String getValue() {
+		return "Hello";
+	}
+	@GetMapping(value="/getname")
+	public String getName() {
+		return "Radhakrishnan";
+	}
+	@GetMapping(value="/getage")
+	public int getAge() {
+		return 21;
+	}
+
+}
